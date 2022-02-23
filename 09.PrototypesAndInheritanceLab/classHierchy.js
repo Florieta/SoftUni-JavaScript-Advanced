7109c21741be64d28525cf36755419a84c372e11
Florieta <florieta.pencheva@gmail.com>
2022-02-23 21:23:01 +0200

    Added all problem solutions about Prototypes Lab

---
+++ 09.PrototypesAndInheritanceLab/classHierchy.js
@@ -0,0 +1,29 @@
+function solve() {
+    class Figure {
+      constructor(units = 'cm') {
+        this.units = units;
+      }
+    }
+  
+    return {
+      Figure,
+      Circle,
+      Rectangle,
+    };
+  }
+
+  const c = new Circle(5);
+console.log(c.area); // 78.53981633974483
+console.log(c.toString()); // Figures units: cm Area: 78.53981633974483 - radius: 5
+
+const r = new Rectangle(3, 4, 'mm');
+console.log(r.area); // 1200
+console.log(r.toString()); //Figures units: mm Area: 1200 - width: 30, height: 40
+
+r.changeUnits('cm');
+console.log(r.area); // 12
+console.log(r.toString()); // Figures units: cm Area: 12 - width: 3, height: 4
+
+c.changeUnits('mm');
+console.log(c.area); // 7853.981633974483
+console.log(c.toString()); // Figures units: mm Area: 7853.981633974483 - radius: 50
